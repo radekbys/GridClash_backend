0ee24d30638b6e0cf98c317aae94d8e49de0c704
radekbys <radekbry12@gmail.com>
2025-05-26 15:33:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express')
+const app = express()
+
+// Define a port
+const PORT = process.env.PORT || 3000
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
