b00059306bf95c93bdffd084823961d3b6c095e7
radekbys <radekbry12@gmail.com>
2025-05-27 14:58:21 +0200

    added starting new game

--- main.js
+++ main.js
@@ -3,11 +3,26 @@ const express = require('express')
 const app = express()
 
 // Define a port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
+
+let gameId = 0
+const games = []
 
 // Define a basic route
 app.get('/', (req, res) => {
   res.send('Hello, World!')
+})
+
+app.get('/start', (req, res) => {
+  const id = gameId
+  gameId += 1
+  const game = {
+    id: id,
+    winner: undefined,
+    log: []
+  }
+  games.push(game)
+  res.json(game)
 })
 
 // Start the server
